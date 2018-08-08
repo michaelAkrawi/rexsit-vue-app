1e836306e076d61933e0a7eb955620f104963350
michael <akrawimichael@gmail.com>
2018-08-08 17:48:58 +0300

    configuring routes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: Home },
+  
+
+
+];
+
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history'
+});
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  el: '#app', 
+  render: h => h(App),
+  router  
+}).$mount("#app")
