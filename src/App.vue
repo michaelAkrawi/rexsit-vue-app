23b559667dcd96826e0c01125f2ba4284281f1e8
michael <akrawimichael@gmail.com>
2018-08-23 16:22:57 +0300

    set hero image on home vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template >
   <div id="app"  dir="rtl">
-    <nav class="navbar navbar-expand-lg">
-       <a class="navbar-brand" href="#">Rexsit</a>
+    <header :class="{'home' : isHomeView}">
+      <nav class="navbar navbar-expand-lg">        
+        <router-link class="navbar-brand" v-bind:to="'/'">Rexsit</router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
    
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-   
-    <div class="collapse navbar-collapse navbar-left" id="navbarNav">
-      <div class="navbar-nav">          
+        <div class="collapse navbar-collapse navbar-left" id="navbarNav">
+          <div class="navbar-nav">          
             <router-link class="nav-item nav-link" v-bind:to="'/sign-up'">{{$t("register")}}</router-link>                            
             <router-link class="nav-item nav-link" v-bind:to="'/login'"> {{$t("login")}} </router-link>          
-      </div>      
-    </div>
-    </nav>
+          </div>      
+        </div>
+      </nav>
+    </header>
+            
     <div class="container">
         <router-view></router-view>
     </div>
@@ -26,15 +28,47 @@
 
 <script>
 export default {
-  name: "app"
+  name: "app",
+  data: function() {
+    return {
+      headerClass: ''
+    };
+  },
+  computed: {
+    isHomeView() {
+      return this.$route.path === '/';
+    }
+  },
+  created() {
+    this.headerClass = 'header-no-image';
+  }
 };
 </script>
 
 <style>
+header {
+  background-image: unset;
+  height: unset;
+}
+
+header.home {
+  background-image: url("./assets/hero_dog_blur.jpg");
+  height: 500px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 35% 75%;
+}
+
 body {
   background-color: #f7f7f7;
   color: #333;
   font-size: 14px;
+}
+
+header .navbar {
+  background-color: #fff;
+  border-color: #e7e7e7;
+  box-shadow: 0 0 10px grey;
 }
 
 .btn-primary {
@@ -64,12 +98,6 @@ body {
 
 .navbar-nav a:first-child {
   border-left: 2px solid #333;
-}
-
-.navbar {
-  background-color: #fff;
-  border-color: #e7e7e7;
-  box-shadow: 0 0 10px grey;
 }
 
 .navbar-left {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,17 +7,33 @@
 </template>
 
 <script>
-
-
-import SearchBar from '../components/SearchBar.vue'
+import SearchBar from "../components/SearchBar.vue";
 
 export default {
-    name : 'app', 
-    components : {
-        "search-bar" : SearchBar
-    }
+  name: "app",
+  components: {
+    "search-bar": SearchBar
+  }
+};
+</script>
+
+
+<style scoped>
+header {
+  background-image: url("../assets/hero_dog_blur.jpg");
+  height: 500px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 35% 75%;
 }
 
+header .navbar {
+  border: none;
+  background: transparent;
+  box-shadow: none;
+}
 
-</script>
+</style>
+
+
 
