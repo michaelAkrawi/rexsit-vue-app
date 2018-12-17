833d0ea4a2ca89e34e65160591f899d20697e15c
michael <akrawimichael@gmail.com>
2018-12-17 16:52:26 +0200

    add more functinality to city component

--- src/components/CItyAutoComplete.vue
+++ src/components/CItyAutoComplete.vue
@@ -1,19 +1,25 @@
 
 <template>
-    <div>
-        <div class="city-search-container">
-          <span>
-            <i class="fas fa-map-marker-alt"></i>
-          </span>
-          <input type="text" v-model="inputText" v-on:change="getCity" placeholder="שם העיר">
-        </div>  
+  <div>
+    <div class="city-search-container">
+      <span v-show="showIcon">
+        <i class="fas fa-map-marker-alt"></i>
+      </span>
+      <input
+        type="text"
+        v-model="inputText"
+        v-on:change="getCity"
+        :class="[{'city-text-show-icon' : showIcon},'form-control']"
+        :placeholder="getPlaceholderText"
+      >
+    </div>
 
-        <ul id="city-search-results" v-show="showSearchResults">
-          <li v-for="c in cities">
-            <a @click="setSelectedCityText(c.name)">{{c.name}}</a>
-          </li>
-        </ul>                                
-    </div>                    
+    <ul id="city-search-results" v-show="showSearchResults">
+      <li v-for="c in cities">
+        <a @click="setSelectedCityText(c.name)">{{c.name}}</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -21,13 +27,21 @@ import { apihelper } from "../scripts/apihelper.js";
 
 export default {
   name: "city-autocomplete",
+  props: ["value", "showIcon", "showPlaceholder"],
   data: function() {
     return {
       inputText: "",
       cities: [],
       showSearchResults: false,
-      lastQuery: ""
+      lastQuery: "",
+      placeholder: ""
     };
+  },
+
+  computed: {
+    getPlaceholderText() {
+      if (this.showPlaceholder) return this.$t("city");
+    }
   },
   watch: {
     inputText: function(newQuery, oldQuery) {
@@ -41,7 +55,7 @@ export default {
     this.debouncedGetCity = _.debounce(this.getCity, 500); //use lodash to delay fetch call
   },
   methods: {
-    getCity: function() {     
+    getCity: function() {
       self = this;
       self.showSearchResults = true;
       if (this.inputText != "") {
@@ -61,6 +75,12 @@ export default {
       this.lastQuery = text;
       this.inputText = text;
       this.showSearchResults = false;
+      this.$emit("input", this.inputText);
+    }
+  },
+  mounted() {   
+    if (this.value !== "") {
+      this.inputText = this.value;
     }
   }
 };
@@ -72,6 +92,10 @@ export default {
   top: 15px;
   right: 24px;
   display: inline-block;
+}
+
+.city-search-container input[type="text"]:focus {
+  outline: none !important;
 }
 
 #city-search-results {
@@ -95,6 +119,10 @@ export default {
 
 #city-search-results li:hover {
   background-color: #ddd;
+}
+
+.city-text-show-icon {
+  padding-right: 35px;
 }
 </style>
 
